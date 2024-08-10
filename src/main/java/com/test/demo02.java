efb653d74376dc515902235a6b7d76cef3189166
fff <aaa@gmail.com>
2024-08-10 18:13:18 +0800

    inti

---
+++ src/main/java/com/test/demo02.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @Author: Func0
+ * @Date: 2024/8/10 18:12
+ */
+public class demo02 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
